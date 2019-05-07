a1d51e43b8c219f31f60e19babb25ff4aaaa6637
Renan Torres <renan.torres@sigma.com.br>
2019-05-07 18:40:00 -0400

    remove objetos não utilizados

--- src/components/projects/projectDetails.js
+++ src/components/projects/projectDetails.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
-import { Redirect } from 'react-router-dom'
 
 const ProjectDetails = (props) => {
     console.log('proppps', props)
